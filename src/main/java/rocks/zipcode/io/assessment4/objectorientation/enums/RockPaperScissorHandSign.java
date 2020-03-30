fcc06e4ded08ebd8c332311780fbd855d72ec162
Dave Kelly <kelly.david.t@gmail.com>
2020-03-30 11:36:13 -0400

    object orientation complete

--- src/main/java/rocks/zipcode/io/assessment4/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/assessment4/objectorientation/enums/RockPaperScissorHandSign.java
@@ -4,13 +4,29 @@ package rocks.zipcode.io.assessment4.objectorientation.enums;
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    ROCK, PAPER, SCISSOR;
+
 
     public RockPaperScissorHandSign getWinner() {
+        if (this==ROCK){
+            return PAPER;
+        } else if (this == PAPER){
+            return SCISSOR;
+        } else if (this == SCISSOR){
+            return ROCK;
+        }
         return null;
     }
 
+
     public RockPaperScissorHandSign getLoser() {
+        if (this==ROCK){
+            return SCISSOR;
+        } else if (this == PAPER){
+            return ROCK;
+        } else if (this == SCISSOR){
+            return PAPER;
+        }
         return null;
     }
 }
