7e519675d862f6aafe9b1781cde2f9a28eb31ca3
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-03-30 11:34:20 -0400

    all tests pass

--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
@@ -1,23 +1,41 @@
 package rocks.zipcode.io.assessment4.fundamentals;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class VowelUtils {
+
     public static Boolean hasVowels(String word) {
-        return null;
+        String[] vowels = {"a", "A", "e", "E", "i", "I", "o", "O", "u", "U"};
+        return Arrays.stream(vowels).parallel().anyMatch(word::contains);
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        char[] ch = word.toCharArray();
+        for (int i = 0; i < ch.length-1; i++) {
+            if(isVowel(ch[i])){
+                return i;
+            }
+        }
+        return -1;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        return isVowel(word.charAt(0));
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        character = Character.toLowerCase(character) ;
+        if (character == 'a' || character == 'e' || character == 'i'
+                || character == 'o' || character == 'u')
+            return true;
+        return false;
     }
 }
