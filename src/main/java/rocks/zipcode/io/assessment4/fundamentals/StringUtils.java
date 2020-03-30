456b0ed4eb04cee8b93d4e673114c2ed93b631fa
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-30 09:45:31 -0400

    saving progress

--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
@@ -1,22 +1,53 @@
 package rocks.zipcode.io.assessment4.fundamentals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+        String toReturn = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i != indexToCapitalize) {
+                toReturn += str.substring(i, i+1);
+            } else
+            toReturn += str.substring(i, i+1).toUpperCase();
+        }
+        return toReturn;
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+        char[] arr = baseString.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (i == indexOfString && arr[i] == characterToCheckFor) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+        List<String> arrList = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j <= string.length(); j++) {
+                arrList.add(string.substring(i,j));
+                count++;
+                System.out.println(string.substring(i,j));
+            }
+        }
+
+        String[] arr = new String[arrList.size()];
+        for (int i = 0; i < arrList.size(); i++) {
+            arr[i] = arrList.get(i);
+        }
+
+        return arr;
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+        return input.length() * ((input.length() + 1) / 2 ) - 1;
     }
 }
