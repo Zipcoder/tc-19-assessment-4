a4b286a79d9fdb4c97de028e28e448440cd6c91f
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-03-30 12:47:17 -0400

    ttt done

--- src/main/java/rocks/zipcode/io/assessment4/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/assessment4/arrays/TicTacToe.java
@@ -4,34 +4,61 @@ package rocks.zipcode.io.assessment4.arrays;
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    public String[][] board;
 
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        return board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+
+        return new String[]{board[0][value],board[1][value],board[2][value]};
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+
+        if (board[rowIndex][0] ==  board[rowIndex][1] && board[rowIndex][1] == board[rowIndex][2]) {
+
+            return true;
+        }
+        return false;
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+
+        if (board[0][columnIndex] ==  board[1][columnIndex] && board[1][columnIndex] == board[2][columnIndex]) {
+
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
+
+        for(int i = 0; i < 3; i++) {
+            if (isColumnHomogeneous(i)) {
+                return board[0][i];
+            }
+            if (isRowHomogenous(i)) {
+                return board[i][0];
+            }
+        }
+        if(board[0][0].equals(board[1][1]) && (board[1][1].equals(board[2][2]))) {
+            return board[0][0];
+        }   else if(board[0][2].equals(board[1][1]) && (board[1][1].equals(board[2][0]))) {
+            return board[0][2];
+        }
         return null;
     }
 
     public String[][] getBoard() {
-        return null;
+        return board;
     }
 }
