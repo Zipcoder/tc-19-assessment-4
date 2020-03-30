124b2621ceda02bde59b50a3a4fab50338e2daba
Dave Kelly <kelly.david.t@gmail.com>
2020-03-30 14:17:37 -0400

    all generics passing

--- src/main/java/rocks/zipcode/io/assessment4/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/assessment4/generics/ArrayUtility.java
@@ -1,9 +1,13 @@
 package rocks.zipcode.io.assessment4.generics;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 /**
  * @author leon on 09/12/2018.
@@ -43,8 +47,16 @@ public class ArrayUtility<SomeType> {
         return occurrences;
     }
 
+
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
-        //return Arrays.stream(array).filter(predicate::apply).toArray();
-        return null;
+        int size = Arrays.stream(array).filter(predicate::apply).toArray().length;
+        SomeType[] ans = (SomeType[]) Array.newInstance(array[0].getClass(), size);
+        List<SomeType> list = Arrays.stream(array).filter(predicate::apply).collect(Collectors.toList());
+        int counter = 0;
+        for (SomeType st : list){
+                ans[counter] = st;
+            counter++;
+        }
+        return ans;
     }
 }
