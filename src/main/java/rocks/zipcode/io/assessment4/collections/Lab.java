adf9183b93319dc1183e18cabb664488e0f246b4
aartikansal <aartilumb@gmail.com>
2020-03-30 09:55:03 -0400

    lab and labstatus complete

--- src/main/java/rocks/zipcode/io/assessment4/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/assessment4/collections/Lab.java
@@ -4,21 +4,30 @@ package rocks.zipcode.io.assessment4.collections;
  * @author leon on 10/12/2018.
  */
 public class Lab {
+    String labName;
+        LabStatus labStatus;
+
     public Lab() {
         this(null);
     }
 
     public Lab(String labName) {
+        this.labName= labName;
+
+
     }
 
     public String getName() {
-        return null;
+        return this.labName;
     }
 
     public void setStatus(LabStatus labStatus) {
+        this.labStatus=labStatus;
+
     }
 
     public LabStatus getStatus() {
-        return null;
+        return this.labStatus;
     }
 }
+
