f50b6a3e6e6b56a894fe17bc9994edb2bb117ea9
aartikansal <aartilumb@gmail.com>
2020-03-30 10:54:58 -0400

    more tests passed

--- src/main/java/rocks/zipcode/io/assessment4/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/assessment4/generics/ArrayUtility.java
@@ -1,5 +1,8 @@
 package rocks.zipcode.io.assessment4.generics;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.function.Function;
 
 /**
@@ -7,21 +10,62 @@ import java.util.function.Function;
  */
 public class ArrayUtility<SomeType> {
     private final SomeType[] array;
-
     public ArrayUtility(SomeType[] array) {
         this.array = array;
     }
 
     public SomeType findOddOccurringValue() {
+        HashMap<SomeType, Integer> occurrence = new HashMap<>();
+        for (SomeType currentKey : this.array) {
+            if (occurrence.containsKey(currentKey)) {
+                int currentValue = occurrence.get(currentKey);
+                occurrence.replace(currentKey, currentValue + 1);
+            } else {
+                occurrence.put(currentKey, 1);
+            }
+        }
+        Iterator<Map.Entry<SomeType, Integer>> iter = occurrence.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<SomeType, Integer>
+                    currentEntry = iter.next();
+            if (currentEntry.getValue() % 2 != 0) {
+                return currentEntry.getKey();
+            }
+        }
         return null;
     }
+    public SomeType findEvenOccurringValue () {
+        HashMap<SomeType, Integer> occurrence = new HashMap<>();
+        for (SomeType currentKey : this.array) {
+            if (occurrence.containsKey(currentKey)) {
+                int currentValue = occurrence.get(currentKey);
+                occurrence.replace(currentKey, currentValue + 1);
+            } else {
+                occurrence.put(currentKey, 1);
+            }
+        }
+        Iterator<Map.Entry<SomeType, Integer>> iter = occurrence.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<SomeType, Integer>
+                    currentEntry = iter.next();
+            if (currentEntry.getValue() % 2 == 0) {
 
-    public SomeType findEvenOccurringValue() {
+                return currentEntry.getKey();
+            }
+        }
         return null;
-    }
 
+
+    }
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
-        return null;
+            int count =0;
+            for(SomeType currentItem : array)
+            {
+                if(currentItem == valueToEvaluate )
+                    count++;
+            }
+
+            return count;
     }
 
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
