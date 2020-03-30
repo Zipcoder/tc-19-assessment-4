a5f6384617babc64f21ce49d95b2ac8213a4fd37
Mo Russ <mauriceruss302@gmail.com>
2020-03-30 09:23:54 -0400

    getAllSubstrings and getNumberOfSubstrings tests passed

--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
@@ -1,5 +1,8 @@
 package rocks.zipcode.io.assessment4.fundamentals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 09/12/2018.
  */
@@ -12,6 +15,7 @@ public class StringUtils {
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
         String lbaseString = baseString.toLowerCase();
+        //String lcharacterToCheckFor = characterToCheckFor.toLowerCase();
         if(lbaseString.indexOf(indexOfString) == characterToCheckFor){
             return true;
         }
@@ -19,10 +23,18 @@ public class StringUtils {
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i+1; j <= string.length(); j++) {
+                set.add(string.substring(i,j));
+            }
+        }
+        return set.toArray(new String[0]);
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+        int answer = input.length();
+
+        return answer * (answer + 1) / 2 -1;
     }
 }
