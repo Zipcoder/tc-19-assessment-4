e90285788816cb88f9dd49fdaf5710410fb5d6a5
vle <obentoka@gmail.com>
2020-03-30 09:53:55 -0400

    refactored vowels

--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
@@ -1,37 +1,40 @@
 package rocks.zipcode.io.assessment4.fundamentals;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class VowelUtils {
-    private static String vowels = "aeiou";
 
     public static Boolean hasVowels(String word) {
-        for (int i = 0; i < word.length(); i++) {
-            if(vowels.contains("" + word.toLowerCase().charAt(i)))
-                return true;
-        }
-        return false;
+        Pattern pattern = Pattern.compile("(?i)[aieou]");
+        Matcher matcher = pattern.matcher(word);
+        return matcher.find();
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        for (int i = 0; i < word.length(); i++) {
-            if(isVowel(word.charAt(i)))
-                return i;
-        }
+        Pattern pattern = Pattern.compile("[aAeEiIoOuU]");
+        Matcher matcher = pattern.matcher(word);
+        if(matcher.find())
+            return matcher.start();
         return -1;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        if(getIndexOfFirstVowel(word) == 0)
-            return true;
+        Pattern pattern = Pattern.compile("[aAeEiIoOuU]");
+        Matcher matcher = pattern.matcher(word);
+        if(matcher.find())
+            if(matcher.start() == 0)
+                return true;
         return false;
     }
 
     public static Boolean isVowel(Character character) {
-        if(vowels.contains(character.toString().toLowerCase()))
-            return true;
-        return false;
+        Pattern pattern = Pattern.compile("[aAeEiIoOuU]");
+        Matcher matcher = pattern.matcher(character.toString());
+        return matcher.find();
     }
 }
