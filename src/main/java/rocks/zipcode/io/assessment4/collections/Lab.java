56318e63d07adea0aef5670500cfe9f99b04852d
Adam Bennett <nyoxidetwitter@gmail.com>
2020-03-30 11:07:18 -0400

    completed

--- src/main/java/rocks/zipcode/io/assessment4/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/assessment4/collections/Lab.java
@@ -3,22 +3,34 @@ package rocks.zipcode.io.assessment4.collections;
 /**
  * @author leon on 10/12/2018.
  */
-public class Lab {
+public class Lab implements Comparable<Lab> {
+
+    private final String labName;
+    private LabStatus status;
+
     public Lab() {
         this(null);
     }
 
     public Lab(String labName) {
+        this.labName = labName;
     }
 
     public String getName() {
-        return null;
+        return (labName != null) ? labName : "";
     }
 
     public void setStatus(LabStatus labStatus) {
+        this.status = labStatus;
     }
 
     public LabStatus getStatus() {
-        return null;
+        return status;
     }
+
+    @Override
+    public int compareTo(Lab o) {
+        return this.status.compareTo(o.getStatus());
+    }
+
 }
--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
@@ -1,23 +1,56 @@
 package rocks.zipcode.io.assessment4.fundamentals;
 
+import java.util.*;
+import java.util.stream.*;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class VowelUtils {
+
+    private static ArrayList<Character> vowels;
+
+    static {
+        vowels = new ArrayList<>();
+        vowels.add('A');
+        vowels.add('E');
+        vowels.add('I');
+        vowels.add('O');
+        vowels.add('U');
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+    }
+
     public static Boolean hasVowels(String word) {
-        return null;
+        List<Character> chars = word.chars().mapToObj(c -> (char)c).collect(Collectors.toList());
+        for (Character c : vowels) {
+            if (chars.contains(c)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        int index = 0;
+        for (char c : word.toCharArray()){
+            if (isVowel(c)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        return isVowel(word.charAt(0));
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        return (character != null) && vowels.contains(character);
     }
 }
