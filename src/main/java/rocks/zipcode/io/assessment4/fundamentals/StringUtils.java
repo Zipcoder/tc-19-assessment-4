01f20298744ca3eb8f3703d8999360ab59e47778
sandeep <snaraya@zipcodes-MBP-10.fios-router.home>
2020-03-30 10:43:58 -0400

    Complete fundamentals and generics

--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
@@ -1,22 +1,40 @@
 package rocks.zipcode.io.assessment4.fundamentals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class StringUtils {
+
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+
+        String[] strArray = str.split("");
+        strArray[indexToCapitalize] = strArray[indexToCapitalize].toUpperCase();
+        return String.join("", strArray);
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+
+        return baseString.charAt(indexOfString)==(characterToCheckFor);
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+
+        Set<String> result = new HashSet<String>();
+
+        for (int i = 0; i <= string.length(); i++) {
+            for (int j = i + 1; j <= string.length(); j++) {
+                result.add(string.substring(i, j));
+            }
+        }
+            String [] toReturn = new String[result.size()];
+            return result.toArray(toReturn);
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+
+            return getAllSubStrings(input).length;
     }
 }
--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
@@ -5,19 +5,49 @@ package rocks.zipcode.io.assessment4.fundamentals;
  */
 public class VowelUtils {
     public static Boolean hasVowels(String word) {
-        return null;
+
+        String[] vowels = {"a", "e", "i", "o", "u"};
+        String[] wordArray = word.toLowerCase().split("");
+        for (int i = 0; i < vowels.length; i++) {
+            for (int j = 0; j < wordArray.length; j++) {
+                if (vowels[i].equals(wordArray[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+
+        String[] vowels = {"a", "e", "i", "o", "u"};
+        String[] wordArray = word.toLowerCase().split("");
+        for (int j = 0; j < wordArray.length; j++) {
+            for (int i = 0; i < vowels.length; i++) {
+                if (vowels[i].equals(wordArray[j])) {
+                    return j;
+                }
+            }
+        }
+        return -1;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+
+        String[] vowels = {"a", "e", "i", "o", "u"};
+        String[] wordArray = word.toLowerCase().split("");
+        for (int i = 0; i < vowels.length; i++) {
+                if (vowels[i].equals(wordArray[0])) { return true;}
+            }
+        return false;
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
+            return true;
+        }
+        return false;
     }
 }
