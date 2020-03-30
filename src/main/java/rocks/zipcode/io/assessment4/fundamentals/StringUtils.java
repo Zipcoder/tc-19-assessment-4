650ae63842d5ed5a19a68bfbcae7acf4ce658e9c
chipfody <60615532+chipfody@users.noreply.github.com>
2020-03-30 10:11:49 -0400

    Save3

--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
@@ -1,22 +1,41 @@
 package rocks.zipcode.io.assessment4.fundamentals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+
+        return str.substring(0, indexToCapitalize) + str.substring(indexToCapitalize, indexToCapitalize + 1).toUpperCase() + str.substring(indexToCapitalize + 1);
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+        return baseString.charAt(indexOfString) == characterToCheckFor;
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+        List<String> subStrings = new ArrayList<> ();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j < string.length() + 1; j++){
+                if (!subStrings.contains(string.substring(i, j)))
+                    subStrings.add(string.substring(i, j));
+            }
+        }
+        return subStrings.toArray(new String[0]);
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+        List<String> subStrings = new ArrayList<>();
+        Integer counter = -1;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j < input.length() + 1; j++) {
+                if (!subStrings.contains(input.substring(i, j)))
+                    subStrings.add(input.substring(i, j));
+                counter++;
+            }
+        }return counter;
     }
 }
--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
@@ -5,19 +5,43 @@ package rocks.zipcode.io.assessment4.fundamentals;
  */
 public class VowelUtils {
     public static Boolean hasVowels(String word) {
-        return null;
+        Boolean vowels = false;
+        for (int i = 0; i < word.length (); i++) {
+            if (word.charAt ( i ) == 'a' || word.charAt ( i ) == 'e' || word.charAt ( i ) == 'i' || word.charAt ( i ) == 'o' || word.charAt ( i ) == 'u') {
+                vowels = true;
+            }
+        }
+        return vowels;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        Integer vowelAt = -1;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') {
+                vowelAt = i;
+                break;
+            }
+        } return vowelAt;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        Boolean startWith = false;
+        if (word.toUpperCase ().charAt(0) == 'A' || word.charAt(0) == 'E' || word.charAt(0) == 'I' || word.charAt(0) == 'O' || word.charAt(0) == 'U') {
+            startWith = true;
+        }
+        return startWith;
     }
+//        if (word.matches ( "\b[aeiouAEIOU]" )) {
+//            startWith = true;
+//        } return startWith;
+
 
     public static Boolean isVowel(Character character) {
-        return null;
+        Boolean isAVowel = false;
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+        } isAVowel = true;
+
+        return isAVowel;
     }
 }
