d1e809cdd3f41ce148690b00173d129b6be5e359
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-03-30 13:10:42 -0400

    completed

--- src/main/java/rocks/zipcode/io/assessment4/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/assessment4/generics/ArrayUtility.java
@@ -16,19 +16,48 @@ public class ArrayUtility<SomeType> {
     }
 
     public SomeType findOddOccurringValue() {
-       return null;
-}
+        SomeType result = null;
+        for (int i = 0; i < array.length; i++) {
+            if ((getNumberOfOccurrences(array[i]) % 2) != 0) {
+                result = array[i];
+            }
+        }
+        return result;
+    }
+
 
     public SomeType findEvenOccurringValue() {
-       return null;
+        SomeType result = null;
+        for (int i = 0; i < array.length; i++) {
+            if ((getNumberOfOccurrences(array[i]) % 2) == 0) {
+                result = array[i];
+            }
+        }
+        return result;
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
-       return null;
+        Integer counter = 0;
+
+        for(int i = 0; i < array.length; i++) {
+            if (array[i] == valueToEvaluate) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
-       return null;
+        List<SomeType> arr = new ArrayList<>(Arrays.asList(array));
+        arr.clear();
+        int i = 0;
+        for(SomeType t:array){
+            if(predicate.apply(t))
+                arr.add(array[i]);
+            i++;
+        }
+        SomeType[] newCopy = Arrays.copyOf(array,arr.size());
+        return arr.toArray(newCopy);
     }
 }
 
