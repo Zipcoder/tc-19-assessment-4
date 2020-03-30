0f05a536a98d9fb79f305dde9d711f1f6dcdd5ed
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-03-30 09:37:42 -0400

    vowels done

--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
@@ -5,19 +5,48 @@ package rocks.zipcode.io.assessment4.fundamentals;
  */
 public class VowelUtils {
     public static Boolean hasVowels(String word) {
-        return null;
+
+        String wordUp = word.toUpperCase();
+        char[] words = wordUp.toCharArray();
+
+        for (int i = 0; i < words.length; i++) {
+            char temp = words[i];
+
+            if (temp == 'A' || temp == 'E' || temp == 'I' || temp == 'O' || temp == 'U') {
+
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+
+        for (int i = 0; i < word.length(); i++) {
+
+            char temp = word.charAt(i);
+
+            if ("AEIOUaeiou".indexOf(temp) != -1)
+                return word.indexOf(temp);
+        }
+        return -1;
     }
 
-
     public static Boolean startsWithVowel(String word) {
-        return null;
+
+        return isVowel(word.charAt(0));
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+
+        if (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U' ||
+                character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+
+            return true;
+        }
+
+        return false;
     }
+
+
 }
