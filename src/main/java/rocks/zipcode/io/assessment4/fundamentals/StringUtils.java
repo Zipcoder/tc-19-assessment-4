36a107cb488091f29ed2913a7a22ef96caab245e
k-romero <imkevinromero@gmail.com>
2020-03-30 10:20:21 -0400

    optimize

--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
@@ -25,24 +25,16 @@ public class StringUtils {
     }
 
     public static String[] getAllSubStrings(String string) {
-        String toWorkWith = string;
         ArrayList<String> resultList = new ArrayList<>();
-        for (int i = 0; i < toWorkWith.length(); i++) {
-            for (int j = i+1; j <= toWorkWith.length() ; j++) {
-                resultList.add(toWorkWith.substring(i,j));
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i+1; j <= string.length(); j++) {
+                String sub = string.substring(i,j);
+                if(!resultList.contains(sub)){
+                    resultList.add(sub);
+                }
             }
         }
-        ArrayList<String> newResult = new ArrayList<>();
-        for (String s : resultList) {
-            if(!newResult.contains(s)){
-                newResult.add(s);
-            }
-        }
-        String[] result = new String[newResult.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = newResult.get(i);
-        }
-        return result;
+        return resultList.toArray(new String[0]);
     }
 
     public static Integer getNumberOfSubStrings(String input){
