d4359e4b103c43bb98a20dfaa5fa67aa4ba9b77a
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-03-30 10:21:12 -0400

    rps done

--- src/main/java/rocks/zipcode/io/assessment4/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/assessment4/objectorientation/enums/RockPaperScissorHandSign.java
@@ -4,13 +4,35 @@ package rocks.zipcode.io.assessment4.objectorientation.enums;
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    //ADD_ENUMERATIONS_HERE;
 
-    public RockPaperScissorHandSign getWinner() {
-        return null;
+    ROCK("ROCK"),
+    PAPER("PAPER"),
+    SCISSOR("SCISSOR");
+    String move;
+
+    RockPaperScissorHandSign(String move) {
+        this.move = move;
     }
 
+    public RockPaperScissorHandSign getWinner() {
+
+        if (move.equals("ROCK"))
+            return PAPER;
+        else if(move.equals("PAPER"))
+            return SCISSOR;
+        else
+            return ROCK;
+    }
+
+
     public RockPaperScissorHandSign getLoser() {
-        return null;
+
+        if (move.equals("ROCK"))
+            return SCISSOR;
+        else if(move.equals("PAPER"))
+            return ROCK;
+        else
+            return PAPER;
     }
 }
