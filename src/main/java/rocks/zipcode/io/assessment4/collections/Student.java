8634bae547ad98a766ecf323e62de0e44253ee37
chipfody <60615532+chipfody@users.noreply.github.com>
2020-03-30 09:43:23 -0400

    Save3

--- src/main/java/rocks/zipcode/io/assessment4/collections/Student.java
+++ src/main/java/rocks/zipcode/io/assessment4/collections/Student.java
@@ -1,11 +1,14 @@
 package rocks.zipcode.io.assessment4.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author leon on 10/12/2018.
  */
 public class Student {
+    List<Lab> labs = new ArrayList<> (  );
+    String labName;
 
     public Student() {
         this(null);
@@ -15,16 +18,40 @@ public class Student {
     }
 
     public Lab getLab(String labName) {
-        return null;
+        Lab labFinder = null;
+        for (Lab lab : labs) {
+            if (lab.getName () == labName) {
+                labFinder = lab;
+            }
+        }
+        return labFinder;
     }
 
     public void setLabStatus(String labName, LabStatus labStatus) {
+        if (this.getLab ( labName ) == null) {
+            throw new UnsupportedOperationException ();
+        }
+        getLab(labName ).labStatus = labStatus;
     }
 
+
     public void forkLab(Lab lab) {
+        lab.setStatus(LabStatus.PENDING);
+        labs.add(lab);
     }
 
     public LabStatus getLabStatus(String labName) {
-        return null;
+        return getLab(labName).labStatus;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder labResults = new StringBuilder (  );
+        for (int i = labs.size () - 1; i >= 0; i--) {
+            labResults.append(labs.get ( i ).getName () + " > " + labs.get ( i ).getStatus () + "\n");
+        }
+        System.out.println (labResults);
+        String result = labResults.toString ();
+        return result.trim ();
     }
 }
